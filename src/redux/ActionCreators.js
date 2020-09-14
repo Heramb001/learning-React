0084edb7763d48c5701dcef5a9423fd4178ebd04
Heramb Pendyala <hpend001@odu.edu>
2020-09-13 23:28:14 -0400

    assignment -4

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -43,6 +43,49 @@ export const postComment = (dishId, rating, author, comment) => (dispatch) => {
                 alert('your comment could not be posted \nError: '+error.message)})
 };
 
+
+export const postFeedback = (feedback) => (dispatch) => {
+    const newFeedback = {
+        firstname: feedback.firstname,
+        lastname: feedback.lastname,
+        telnum: feedback.telnum,
+        email: feedback.email,
+        agree: feedback.agree,
+        contactType: feedback.contactType,
+        message: feedback.message
+    }
+    newFeedback.date = new Date().toISOString();
+    return fetch(baseUrl + 'feedback', {
+        method: 'POST',
+        body: JSON.stringify(newFeedback),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin'
+    })
+        .then(response => {
+            if (response.ok){
+                return response;
+            }
+            //recieve a response other than 'ok'
+            else{
+                var error = new Error('Error ' + response.status + ': '+response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(response => {
+            dispatch(alert("Thanks for your feedback!\n"+JSON.stringify(response)));
+        })
+        .catch(error => {console.log('Post Feedback : ',error.message);
+                alert('your feedback could not be posted \nError: '+error.message)})
+};
+
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
 
@@ -152,4 +195,41 @@ export const promosFailed = (errmess) => ({
 export const addPromos = (promos) => ({
     type: ActionTypes.ADD_PROMOS,
     payload: promos
+});
+
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading(true));
+    return fetch(baseUrl + 'leaders')
+        .then(response => {
+            if (response.ok){
+                return response;
+            }
+            //recieve a response other than 'ok'
+            else{
+                var error = new Error('Error ' + response.status + ': '+response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(leaders => dispatch(addLeaders(leaders)))
+        .catch(error => dispatch(leadersFailed(error.message)));
+}
+
+export const leadersLoading = () => ({
+    type:ActionTypes.LEADERS_LOADING
+});
+
+export const leadersFailed = (errmess) => ({
+    type:ActionTypes.LEADERS_FAILED,
+    payload: errmess
+});
+
+export const addLeaders = (leaders) => ({
+    type: ActionTypes.ADD_LEADERS,
+    payload: leaders
 });
